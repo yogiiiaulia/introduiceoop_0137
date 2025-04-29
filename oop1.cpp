7a481fbeab346ac0d42735cb050fea436d1172c6
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-04-29 18:31:54 +0700

    Membuat Procedure printData

--- oop1.cpp
+++ oop1.cpp
@@ -7,7 +7,11 @@ class Mahasiswa{
     string nama;
     float nilai;
 
-    
+    void printData(){
+        cout << "Nim : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
+    }
 };
 int main(){
     
